5ba592f850ebf1c7d3a0337a0e261c9e22066905
ziwu07 <76572511+ziwu07@users.noreply.github.com>
2026-04-07 11:53:31 -0400

    arch defs

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ export F4PGA_INSTALL_DIR="$HOME/repos/f4pga"
 export YOSYS_PREFIX="$CONDA_PREFIX"
 export PATH="$HOME/repos/f4pga/xc7/conda/envs/xc7/bin:$PATH"
 f4pga build -f flow.json "$@"
+# https://storage.googleapis.com/symbiflow-arch-defs/artifacts/prod/foss-fpga-tools/symbiflow-arch-defs/continuous/install/20230411-180123/symbiflow-arch-defs-xc7a50t_test-5e974a8.tar.xz
+# https://storage.googleapis.com/symbiflow-arch-defs/artifacts/prod/foss-fpga-tools/symbiflow-arch-defs/continuous/install/20230411-180123/symbiflow-arch-defs-install-xc7-5e974a8.tar.xz
